d2fd700a1852a1896b724508a695001dbb12d767
madraceee <raajanitheesh@gmail.com>
2024-09-01 21:44:48 +0530

    boot.dev-5 setting up main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		log.Fatal("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		log.Fatal("too many arguments provided")
+		os.Exit(1)
+	}
+
+	log.Printf("starting crawl of: %s\n", args[0])
+	os.Exit(0)
 }
